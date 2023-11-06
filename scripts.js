60408c4b23dbbed2412229893d6421e9a7c4d99f
Dejean Whyte <dejeanwhyte@gmail.com>
2023-11-05 23:53:54 -0500

    Added Pictures, Updated styles and js files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
 /* Add your JavaScript to this file */
+
+window.onload = function(){
+    var format = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    var email = document.getElementById("email");
+    var msg = document.getElementsByClassName("message")[0];
+    var submit = document.getElementsByTagName("button")[0];
+    
+    submit.addEventListener("click", (e)=>{
+        e.preventDefault();
+    
+    if (email.value =="" || !(email.value.match(format))){
+        msg.innerHTML = "Please enter a valid email address.";
+        email.value ="";
+    }
+    
+    else{
+        msg.innerHTML = "Thank you! Your email address " + email.value + " has been added to our mailing list";
+    }
+
+    }
+    )
+    
+}
